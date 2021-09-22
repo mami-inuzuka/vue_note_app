0efa217d8e89de062dfcbf230a91ec92f4fa7466
mami-inuzuka <inuzuka.mami@gmail.com>
2021-09-22 18:47:38 +0900

    チェックボックスを設置

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ const app = new Vue({
       } else {
         this.todoLists.push({
           id: id,
+          status: false,
           text: textToAdd,
           isEditable: false
         })
